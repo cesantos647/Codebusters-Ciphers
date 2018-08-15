422890e870cc8b6d631894e9e6d535f45aff9922
cesantos647 <cesantos647@gmail.com>
2018-08-15 10:02:00 -0700

    Added new file

---
+++ vigenere.js
@@ -0,0 +1,15 @@
+function encode_v() {
+
+	//Gets message that was submitted
+	message = document.getElementById('message').value;
+
+	//Checks if there is a message
+	if(typeof(message) === 'undefined' || message === '') {
+
+		alert('Please submit a message');
+
+		return false;
+
+	}
+
+}
