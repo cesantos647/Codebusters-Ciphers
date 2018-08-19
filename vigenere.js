e1104b4cf323175fe7754cd28ae2566c094e45d3
cesantos647 <cesantos647@gmail.com>
2018-08-18 23:02:24 -0700

    Began implementation of the vigenere cipher encrpytion

--- vigenere.js
+++ vigenere.js
@@ -14,7 +14,11 @@ function encode_v() {
 
 	key = document.getElementById('key').value;
 
-	if(typeof(key) === 'undefined' || message === '' || key.length > message.length) {
+	//var keySpaceCount = key.split(' ').length;
+
+	var keyNoSpaceCount = key.split(' ').join('').split('');
+
+	if(typeof(key) === 'undefined' || message === '' || key.length > message.length || key.length !== keyNoSpaceCount.length) {
 
 		alert('Please submit a key that is shorter than the message');
 
@@ -30,20 +34,89 @@ function encode_v() {
 
 	var i = 0;
 
+	console.log(message.length);
+
+
 	while(key.length !== message.length) {
 
-		i++;
 
-		key.push(key[i % key.length]);
+		key.push(key[i % (key.length-1)]);
+
+		i++;
 
 	}
 
 	console.log(key);
 
+	for(var t = 0; t < key.length; t++) {
+
+		var keyNum = [];
+
+		//Checks the alphabet array until the letter is found
+		for(var a = 0; a < alphabet.length && check === false; a++) {
+
+			//Once the letter is found, the letter is keyed by the amount specified by the user
+			if(key[t] === alphabet[x]) {
+
+				//Encrypts letter
+				keyNum.push(alphabet_number[x]);
+
+				//Cancels the search
+				check = true;
+
+			}
+
+		}
+
+	}
+
+	message = message.split(' ');
+	//For each word
+	for(var z = 0; z < message.length; z++) {
+
+		message[z] = message[z].split('')
+
+		//Encodes message word by word
+		for(y = 0; y < message.length; y++) {
+
+			var messageNum = []
+
+			//Encrypts each letter or number in the word
+			for(var i = 0; i < message[y].length; i++) {
+
+				//Resets the search for each letter in the message
+				var check = false
+
+				//Checks the alphabet array until the letter is found
+				for(var x = 0; x < alphabet.length && check === false; x++) {
+
+					//Once the letter is found, the letter is keyed by the amount specified by the user
+					if(message[y][i] === alphabet[x]) {
+
+						//Encrypts letter
+						messageNum.push(alphabet_number[x]);
+
+						//Cancels the search
+						check = true;
+
+					}
+
+				}
+
+			}
+		
+		}
+
+	}
+
+	console.log(messageNum);
+	console.log(keyNum);
+	//AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+
 }
 
-//Convert key to become as long as the message
-//Convert message into numbers
+//Convert key to become as long as the message DONE
+//Convert message into numbers DONE
 //Convert key into numbers
 //Add message and key together
 //Convert added numbers into letters
